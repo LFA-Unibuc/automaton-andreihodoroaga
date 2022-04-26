00f5b40fe4068ed9dd39d5336ba33c62b209ae52
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2022-04-26 11:41:15 +0300

    refactor for dfa

--- automaton.py
+++ automaton.py
@@ -1,13 +1,15 @@
 class Automaton:
-
     def __init__(self, config_file):
         self.config_file = config_file
 
         automaton = {"sigma": [], "states": [], "transitions": []}
         # create a dictionary containing sigma, states and transitions
         with open(self.config_file) as f:
-            while len(automaton["sigma"]) == 0\
-              or len(automaton["states"]) == 0 or len(automaton["transitions"]) == 0:
+            while (
+                len(automaton["sigma"]) == 0
+                or len(automaton["states"]) == 0
+                or len(automaton["transitions"]) == 0
+            ):
                 line = f.readline().strip()
                 section = line.lower().split(":", maxsplit=1)[0].strip()
                 # ignore comments
@@ -20,14 +22,28 @@ class Automaton:
                         if "..." not in line and "end" not in line.lower():
                             automaton[section].append(line)
         # refactor states
-        automaton["states"] = [tuple([s.strip() for s in state.split(",")]) for state in automaton["states"]]
+        automaton["states"] = [
+            tuple([s.strip() for s in state.split(",")])
+            for state in automaton["states"]
+        ]
+        # refactor transitions from strings to tuples of the form (initial_state:{letter: next_state})
+        refactored_transitions = {}
+        for transition in automaton["transitions"]:
+            transition_components = [item.strip() for item in transition.split(", ")]
+            initial_state, letter, next_state = transition_components
+            try:
+                refactored_transitions[initial_state][letter] = next_state
+            except KeyError:
+                refactored_transitions[initial_state] = {letter: next_state}
+        automaton["transitions"] = refactored_transitions
 
         self.automaton = automaton
-        print(automaton)
 
     def validate(self):
         # check for unique start state and final state
-        states_info = [state[1] if len(state) > 1 else "" for state in self.automaton["states"]]
+        states_info = [
+            state[1] if len(state) > 1 else "" for state in self.automaton["states"]
+        ]
         if states_info.count("S") > 1:
             raise Exception("Only one starting state allowed!")
 
@@ -36,7 +52,11 @@ class Automaton:
             state1, word, state2 = [t.strip() for t in transition.split(",")]
             valid_states = [state[0] for state in self.automaton["states"]]
 
-            if state1 not in valid_states or state2 not in valid_states or word not in self.automaton["sigma"]:
+            if (
+                state1 not in valid_states
+                or state2 not in valid_states
+                or word not in self.automaton["sigma"]
+            ):
                 raise Exception("Transition contains invalid words or states!")
         return True
 
@@ -50,13 +70,8 @@ class Automaton:
 
     def read_input(self, input_str):
         """Return the automaton's final configuration
-        
+
         If the input is rejected, the method raises a
         RejectionException.
         """
         pass
-    
-
-if __name__ == "__main__":
-    a = Automaton('input.txt')
-    print(a.validate())
